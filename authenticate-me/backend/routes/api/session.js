0e4b52962d77e1ad190eabc34cb10d1c32656150
haamraah@yahoo.com <haamraah@yahoo.com>
2023-05-06 23:11:06 -0700

    validation added to login and signup in routes

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -3,11 +3,23 @@ const express = require('express')
 const router = express.Router();
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+// validate credentials and password
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -28,6 +40,7 @@ router.post(
         });
     }
 );
+
 // Restore session user
 router.get(
     '/',
